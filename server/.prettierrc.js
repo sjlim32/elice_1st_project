add40760592c8ec1b51916f5cbc0897c55ce90ba
beomjin97 <qjawls646@gmail.com>
2023-01-31 15:11:05 +0900

    backend

---
+++ server/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "tabWidth": 2,
+  "printWidth": 140,
+  "singleQuote": true,
+  "trailingComma": "es5",
+  "arrowParens": "always",
+  "semi": true
+}
---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('ok');
+});
+
+app.listen(5000, () => {
+  console.log('server is running');
+});
