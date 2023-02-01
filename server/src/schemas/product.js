58ce3e7226b2f3dba64f22a27c9747dd9e3720ad
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-02-01 22:35:48 +0900

    schema, model 생성

---
+++ server/src/schemas/product.js
@@ -0,0 +1,32 @@
+import { model, Schema } from 'mongoose';
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+});
+
+const Product = model('Product', productSchema);
+export default Product;
