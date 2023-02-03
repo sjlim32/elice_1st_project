73e28481d81f30bcee010565e2b17deeef5a6968
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-02-03 16:56:53 +0900

    extract gender from category

--- server/src/schemas/category.js
+++ server/src/schemas/category.js
@@ -1,12 +1,8 @@
 import { model, Schema } from 'mongoose';
 
 const categorySchema = new Schema({
-  gender: {
-    type: [String],
-    required: true,
-  },
   major_classification: {
-    type: [String],
+    type: String,
     required: true,
   },
   minor_classification: {
