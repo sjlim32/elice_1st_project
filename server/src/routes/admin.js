bd283f6b95fe60a796ca04633de238c8cc0de19a
heyhye94 <heyhye94@gmail.com>
2023-02-04 12:00:03 +0900

    Revert "Merge branch 'revert-6a971e59' into 'master'"
    This reverts commit 087f2f53c71ca2d2a73e161c286f3637d83e77e8, reversing
    changes made to 4a9999613a7d290f51e75bd007b3b7d47e6025f5.

--- server/src/routes/admin.js
+++ server/src/routes/admin.js
@@ -1,50 +1,49 @@
 import { Router } from 'express';
-import Category from '../schemas/category.js';
 import Product from '../schemas/product.js';
-import Gender from '../schemas/gender.js';
+
 const router = Router();
 
 /** 상품 추가 */
 router.post('/product', async (req, res) => {
-  const { name, price, image, description, company, gender, major_classification, minor_classification } = req.body;
-  // try-catch 사용하기! 쓰면 좋다!
-  try {
-    const product = await Product.create({
-      name,
-      price,
-      image,
-      description,
-      company,
-      gender,
-      major_classification,
-      minor_classification,
-    });
-    res.json(product);
-  } catch (error) {
-    res.json({ error });
-  }
+    const { name, price, image, description, company, gender, major_classification, minor_classification } = req.body;
+    // try-catch 사용
+    try {
+        const product = await Product.create({
+            name,
+            price,
+            image,
+            description,
+            company,
+            gender,
+            major_classification,
+            minor_classification
+        });
+        res.json(product);
+    } catch (error) {
+        res.json({ error });
+    };
 });
 
 /** 상품 삭제 */
 router.delete('/product/:product_id', async (req, res) => {
-  try {
-    await Product.findByIdAndDelete(req.params.product_id);
-
-    // 객체로 만들기
-    res.json({ message: '삭제 완료되었습니다.' });
-  } catch (error) {
-    res.json({ message: '삭제 실패하였습니다.' });
-  }
+    const productId = req.params;
+    // console.log(productId);
+    try {
+        await Product.deleteOne({ _id: productId.product_id });
+        res.json({ message: '삭제 완료되었습니다.' });
+    } catch (error) {
+        res.json({ message: '삭제 실패하였습니다.' });
+    }
 });
 
 router.patch('/product', async (req, res) => {
-  const { gender, classification } = req.body;
-  await Gender.findOneAndUpdate({}, { gender });
+    const { gender, classification } = req.body;
+    await Gender.findOneAndUpdate({}, { gender });
 
-  const majors = Object.keys(classification);
-  majors.forEach(async (major) => {
-    await Category.findOneAndUpdate({ major_classification: major }, { minor_classification: classification[major] });
-  });
+    const majors = Object.keys(classification);
+    majors.forEach(async (major) => {
+        await Category.findOneAndUpdate({ major_classification: major }, { minor_classification: classification[major] });
+    });
 });
 
 export default router;
