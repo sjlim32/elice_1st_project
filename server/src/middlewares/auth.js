d7cda3ab3b320c8ec620baf5b6eeb49cb0cdb626
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-02-06 12:02:57 +0900

    불필요한 async 제거

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-const auth = async (req, res, next) => {
+const auth = (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(' ')[1];
     let decodedData;
