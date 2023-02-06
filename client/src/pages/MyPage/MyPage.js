949b4993a88f65e1682a02c639a656a550b95f57
ErinSY <seoyul0203@gmail.com>
2023-02-07 01:22:14 +0800

    초기코드 세팅

--- client/src/pages/MyPage/MyPage.js
+++ client/src/pages/MyPage/MyPage.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function MyPage() {
-  return <div>MyPage</div>;
+  const [customerInfo, setCustomerInfo] = useState('');
+
+  useEffect(() => {
+    axios.get('/data/customerInfo.json').then(res => setCustomerInfo(res.data));
+  }, []);
+
+  return (
+    <Container>
+      <WelcomeMsg>
+        <AccountIcon />
+      </WelcomeMsg>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  width: 100vw;
+  margin-top: 90px;
+  padding: 40px;
+`;
+
+const WelcomeMsg = styled.div`
+  width: 100vw;
+  margin-top: 90px;
+  padding: 40px;
+`;
+
+const AccountIcon = styled(AccountCircleIcon)`
+  color: rgba(153, 164, 151, 1);
+  font-size: 50px;
+  width: 50px;
+  height: 50px;
+`;
