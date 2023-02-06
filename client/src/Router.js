cb6bb7731ba7c5ce0efbdc42ef7f38b9ef86aa63
ErinSY <seoyul0203@gmail.com>
2023-02-07 00:59:03 +0800

    Router 업데이트

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup/Signup';
+import LandingPage from './pages/LandingPage/LandingPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import ProductList from './pages/ProductList/ProductList';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/user/login" element={<LoginPage />} />
         <Route path="/user/signup" element={<Signup />} />
+        <Route path="/product" element={<ProductList />} />
         <Route path="/product/:productId" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
