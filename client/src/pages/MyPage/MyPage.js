d126bda075e4117a53fe61a381610651b7309df5
ErinSY <seoyul0203@gmail.com>
2023-02-07 10:17:39 +0800

    DOCS: UI 초기구성

--- client/src/pages/MyPage/MyPage.js
+++ client/src/pages/MyPage/MyPage.js
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
+import Line from '../../components/line';
+import OrderList from './OrderList/OrderList';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function MyPage() {
   const [customerInfo, setCustomerInfo] = useState('');
+  const [orderList, setOrderList] = useState('');
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get('/data/customerInfo.json').then(res => setCustomerInfo(res.data));
@@ -13,8 +19,29 @@ export default function MyPage() {
   return (
     <Container>
       <WelcomeMsg>
-        <AccountIcon />
+        <AccountIcon className="mat-icon" />
+        <p>안녕하세요 {customerInfo.name} 고객님 </p>
+        <p>오늘도 환영합니다!</p>
       </WelcomeMsg>
+      <MenuTab>
+        <ul className="tabs">
+          <li
+            onClick={() => {
+              navigate(`/order/${customerInfo.id}`);
+            }}
+          >
+            주문내역
+          </li>
+          <li>내 정보 관리</li>
+        </ul>
+      </MenuTab>
+      <Subtitle>
+        <p>최근 주문 내역</p>
+        <Line widthLength="60%" />
+      </Subtitle>
+      <ShowList>
+        {orderList.length === 0 ? '최근 주문내역이 없습니다.' : <OrderList />}
+      </ShowList>
     </Container>
   );
 }
@@ -26,14 +53,53 @@ const Container = styled.div`
 `;
 
 const WelcomeMsg = styled.div`
-  width: 100vw;
-  margin-top: 90px;
-  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 70px;
+  > p {
+    font-weight: 500;
+  }
 `;
 
 const AccountIcon = styled(AccountCircleIcon)`
   color: rgba(153, 164, 151, 1);
-  font-size: 50px;
-  width: 50px;
-  height: 50px;
+  font-size: 1000px;
+`;
+
+const MenuTab = styled.div`
+  width: 55%;
+  margin: 70px auto;
+  font-size: 20px;
+  text-align: center;
+  font-weight: 600;
+  > .tabs {
+    display: flex;
+    > li {
+      width: 100%;
+      text-align: center;
+      border: 1px solid #b1b1b1;
+      padding: 20px;
+      cursor: pointer;
+      color: #b1b1b1;
+
+      &:hover {
+        font-wight: 900;
+        border-bottom: none;
+        color: black;
+      }
+    }
+  }
+`;
+
+const Subtitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ShowList = styled.div`
+  padding: 30px;
+  text-align: center;
 `;
