4d0c05ed3b783c5e6c70dece1a9175ce7bbac174
ErinSY <seoyul0203@gmail.com>
2023-02-08 17:19:24 +0800

    라우터 수정

--- client/src/Router.js
+++ client/src/Router.js
@@ -3,22 +3,30 @@ import Signup from './pages/Signup/Signup';
 import LandingPage from './pages/LandingPage/LandingPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import ProductList from './pages/ProductList/ProductList';
-import MyPage from './pages/MyPage/MyPage'
+import MyPage from './pages/MyPage/MyPage';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import Header from './pages/Layout/Header';
 import Footer from './pages/Layout/Footer';
 const Router = () => {
+  const ROUTES = {
+    MAIN: '/',
+    LOGIN: '/user/login',
+    SIGNUP: '/user/signup',
+    PRODUCTLIST: '/user/product',
+    PRODUCTDETAIL: '/user/product/:productId',
+    MYPAGE: '/user/:userid',
+  };
   return (
     <>
       <Header />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/user/login" element={<LoginPage />} />
-          <Route path="/user/signup" element={<Signup />} />
-          <Route path="/product" element={<ProductList />} />
-          <Route path="/product/:productId" element={<ProductDetail />} />
-          <Route path="/user/:userid" element={<MyPage />} />
+          <Route path={ROUTES.MAIN} element={<LandingPage />} />
+          <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+          <Route path={ROUTES.SIGNUP} element={<Signup />} />
+          <Route path={ROUTES.PRODUCTLIST} element={<ProductList />} />
+          <Route path={ROUTES.PRODUCTDETAIL} element={<ProductDetail />} />
+          <Route path={ROUTES.MYPAGE} element={<MyPage />} />
         </Routes>
       </BrowserRouter>
       <Footer>Footer</Footer>
