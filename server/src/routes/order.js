026e3fe0d21a8ee240829b5bd3ff62e7418df33d
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-02-10 14:44:31 +0900

    middleware auth

--- server/src/routes/order.js
+++ server/src/routes/order.js
@@ -1,18 +1,19 @@
 import { Router } from 'express';
 import { createOrder, getOrderDetails, modifyOrderDetails, deleteOrder } from '../controllers/order.js';
+import auth from '../middlewares/auth.js';
 
 const router = Router();
 
 // * 주문 추가
-router.post('/', createOrder);
+router.post('/', auth, createOrder);
 
 // * 주문 조회
-router.get('/:user_id', getOrderDetails);
+router.get('/:user_id', auth, getOrderDetails);
 
 // * 주문 수정
-router.patch('/:order_id', modifyOrderDetails);
+router.patch('/:order_id', auth, modifyOrderDetails);
 
 // * 주문 취소
-router.delete('/:order_id', deleteOrder);
+router.delete('/:order_id', auth, deleteOrder);
 
 export default router;
