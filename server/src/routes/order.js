7d3fdf277c698b03052f6e0fdddf46d83cbff061
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-02-11 02:43:10 +0000

    Update order.js

--- server/src/routes/order.js
+++ server/src/routes/order.js
@@ -5,10 +5,10 @@ import auth from '../middlewares/auth.js';
 const router = Router();
 
 // * 주문 추가
-router.post('/', createOrder);
+router.post('/', auth createOrder);
 
 // * 주문 조회
-router.get('/:user_id', getOrderDetails);
+router.get('/:user_id', auth, getOrderDetails);
 
 // * 주문 수정
 router.patch('/:order_id', auth, modifyOrderDetails);
